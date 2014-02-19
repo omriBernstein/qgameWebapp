d6f18b8b27fa48aed5ea98627d81460d373e0684
Seth Toles <sethtoles@gmail.com>
2014-02-18 22:20:22 -0800

    use clojurescript port of qgame
    still unable to use qgame fully

--- project.clj
+++ project.clj
@@ -1,18 +1,13 @@
-(defproject qromp "0.2.0"
+(defproject qromp "0.3.0"
   :description ""
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cheshire "5.2.0"]
-                 [ring/ring-devel "1.1.8"]
-                 [http-kit "2.0.0"]
-                 [compojure "1.1.5"]
-                 [ring-cors "0.1.0"]
-                 [hiccups "0.3.0"]
-                 [org.clojars.hippiccolo/qgame "0.1.2"]
-                 [com.keminglabs/c2 "0.2.3"]]
-  :plugins [[lein-cljsbuild "0.3.0"]]
-  :cljsbuild {:builds [{:source-paths ["src/qromp/frontend/app/visualizer"]
-                        :compiler {:output-to "src/qromp/frontend/compiled/app.js"
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojars/hippiccolo/qgame "0.2.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-to "static/scripts/app.js"
                                    :optimizations :whitespace
-                                   :pretty-print true}}]}
-  :main qromp.backend.core
+                                   :pretty-print true
+                                   :foreign-libs [{:file "static/scripts/math.min.js"
+                                                   :provides ["math.js"]}]}}]}
   :min-lein-version "2.0.0")
