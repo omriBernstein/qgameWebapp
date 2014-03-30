a38620cccb471649c366b6dc2ef68b394456c2ba
Omri Bernstein <ob08@hampshire.edu>
2014-03-29 21:38:49 -0400

    Oracles work now

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description ""
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2156"]
-                 [org.clojars/hippiccolo/qgame "0.3.2"]]
+                 [org.clojars/hippiccolo/qgame "0.3.4"]]
   :plugins [[lein-cljsbuild "1.0.2"]]
   :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler {:output-to "static/qromp.js"
