476b6ee46e31b110b3ec996e8caab8c5320481ab
knod <knodding@gmail.com>
2014-04-21 10:03:04 -0400

    Gets it working again. Issue - clicking too fast creates problems.

--- static/visualizer.js
+++ static/visualizer.js
@@ -147,31 +147,30 @@ function VisualizerObject(containerID) {
 		}
 
 	// --- ENTANGLEMENT --- //
-	// Need to wait till here so have correct values?
-	// For some reason odd amounts of qubits put the whole thing off center
+	// Need to wait till here so have correct values for now
+
+		var entangMatrix = [];
 
 		// Let's try with d3 again
 		d3.selectAll(".qubit").each(function (dat, indx) {
 			// Assign an array to the qubit object without destroying
 			// the previous data object (entang.js)
 			d3.select(this).data()[0].entang = entang.createRow(indx, numQubits);
-			
-			console.log(d3.select(this).data()[0].entang);
+			// Put that array into the entanglement matrix array
+			entangMatrix.push(d3.select(this).data()[0].entang);
+
 			// d3.select(this).data([0123]); // console.log gets [83]
 		});
 
-		function createMatrix (dat, index) {}
-
-
 		function createEntang () {
-		// Need to wait till the qubits are done animating, animTime. How?
+		// Need to wait till the qubits are done animating, animTime.
 			// Ideally, if the diagram already exists, just transition it...
 			// but for now, destroy the old one and make a new one
 			$(".entang").remove();
 
 			setTimeout(function () {
 				var center = containerWidth/2 + ", " + (containerHeight + yOffset)/2;
-				entang.createChord(matrix, arrangeRadius-qubitRadius, center);}
+				entang.createChord(entangMatrix, arrangeRadius-qubitRadius, center);}
 				, animTime);
 		}
 
